6d24bcff20facc827e3626a874e873d43d047b79
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2022-07-12 22:36:56 -0400

    setup mongodb connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app=express()
+const morgan=require('morgan')
+const mongoose=require('mongoose')
+require ('dotenv').config()
+
+//db connection
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Mongo-DB Connected...'))
+    .catch(err => console.log(err));
+
+//middleware
+if (process.env.NODE_ENV==='development') {
+  app.use(morgan('dev'))
+  console.log('the app is in development phase')
+
+} else if (process.env.NODE_ENV==='production') {
+
+  console.log('the app is in production phase ')
+}
